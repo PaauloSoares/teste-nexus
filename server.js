595d6d3a44e9a30eb1b09bd890bdca3a2673e388
Paulo Soares <paulohfrancelino1@hotmail.com>
2025-08-07 10:11:30 -0300

    correcao server.js port--

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('./src/app.js');
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('servidor escutando!');
+  console.log(`Servidor escutando na porta ${PORT}`);
 });
